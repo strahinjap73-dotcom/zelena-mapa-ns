989f3ea7d15c352d61310fed0cb3189585bffb67
Strahinja <strahinjap73@gmail.com>
2026-06-23 10:36:08 +0200

    fix sa paketima

--- zelena-mapa-backend/src/main/java/com/example/zelenamapabackend/repository/FriendRequestRepository.java
+++ zelena-mapa-backend/src/main/java/com/example/zelenamapabackend/repository/FriendRequestRepository.java
@@ -1,4 +1,4 @@
-package com.example.zelenamapabackend;
+package com.example.zelenamapabackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
@@ -7,5 +7,9 @@ import java.util.Optional;
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
     List<FriendRequest> findByReceiverIdAndStatus(Long receiverId, FriendRequest.Status status);
     List<FriendRequest> findBySenderIdAndStatus(Long senderId, FriendRequest.Status status);
-    Optional<FriendRequest> findBySenderIdAndReceiverIdAndStatus(Long senderId, Long receiverId, FriendRequest.Status status);
+    Optional<FriendRequest> findBySenderIdAndReceiverIdAndStatus(
+        Long senderId,
+        Long receiverId,
+        FriendRequest.Status status
+    );
 }
