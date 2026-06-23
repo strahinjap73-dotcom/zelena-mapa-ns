f3f7e9cbeb181237559d1a0f7230514618a25250
Strahinja <strahinjap73@gmail.com>
2026-06-23 10:43:47 +0200

    fix imports

--- zelena-mapa-backend/src/main/java/com/example/zelenamapabackend/repository/FriendRequestRepository.java
+++ zelena-mapa-backend/src/main/java/com/example/zelenamapabackend/repository/FriendRequestRepository.java
@@ -1,5 +1,6 @@
 package com.example.zelenamapabackend.repository;
 
+import com.example.zelenamapabackend.FriendRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
